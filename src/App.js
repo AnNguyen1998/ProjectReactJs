a2c91a8947b3aa6c0b584763ba147bd21a7d09d9
AnNguyen1998 <conandold@gmail.com>
2024-07-26 11:43:59 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element=""/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
 
 export default function Products() {
   return (
     <div>
-        
+        <Header/>
+            Home
+        <Footer/>
     </div>
   )
 }
