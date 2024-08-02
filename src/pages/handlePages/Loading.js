cd739824d9a0c7f6451349d68ac5c47765bb3f31
AnNguyen1998 <conandold@gmail.com>
2024-08-02 22:02:21 +0700

    thu 6

--- src/pages/handlePages/Loading.js
+++ src/pages/handlePages/Loading.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Spin } from 'antd';
 
 export default function Loading() {
   return (
-        <Spin style={{position:'absolute', top:'50%', left:'50%'}} tip="Loading" size="large"></Spin>
+        <div>
+
+        </div>
   )
 }
